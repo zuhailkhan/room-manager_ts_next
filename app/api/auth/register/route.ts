5cf773290f4b05a3cb2e57a1a04951febb3a3354
zuhailkhan <zuhail.khan@appstone.in>
2023-06-15 18:15:41 +0530

    Added Register Route

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,34 +1,62 @@
+import { NextRequest, NextResponse } from "next/server"
 import User, { IUser } from "@/models/User"
 import { connectDB } from "@/utils/database"
 import bcrypt from 'bcryptjs'
-export const POST = async (request: Request) => {
+import { HydratedDocument } from "mongoose"
+
+export const POST = async (request: NextRequest) => {
     const { username, email, password } = await request.json()
     if([username, email, password].some((value) => !value)) {
-        return new Response(JSON.stringify({ error: 'All fields are required' }), { status: 400 })
+        return NextResponse.json({
+            message: "All fields are required"
+        }, {
+            status: 400
+        })
     }
+
     try {
         await connectDB()
-        const userExists = await User.count( { username } )
-        console.log(userExists)
-        if(!userExists) {
-            bcrypt.hash(password, 10, (err, result) => {
-                if (err) {
-                    throw new Error("Something went wrong")
-                }
-                else if (result) {
-                    const newUser = new User({ username, email, password: result })
-                    newUser.save()
 
-                    return
-                }
+        const user = await User.count({ $or: [ { username }, { email } ] })
+        if(user <= 0) {
+            const hashedPassword = bcrypt.hashSync(password, 10)
+
+            const newUser: HydratedDocument<IUser> = new User({
+                username,
+                email,
+                password: hashedPassword
             })
-            return new Response(JSON.stringify({message: "User created"}), { status: 200 })
+
+            if(!newUser || !hashedPassword) {
+                throw new Error("Something went wrong")
+            }
+
+            const result = await newUser.save()
+            return NextResponse.json({
+                message: "User created successfully",
+                user: {
+                    id: result._id,
+                    username: result.username,
+                    email: result.email
+                }
+            }, {
+                status: 201
+            })
         } else {
-            return new Response(JSON.stringify({error: "User already exists"}), { status: 400 })
+            return NextResponse.json({
+                "message": "User with email/username already exists"
+            }, {
+                status: 400
+            })
         }
-        
     } catch (error) {
-        console.log(error)
-        return new Response(JSON.stringify(error), { status: 500 })
+        console.error(error)
+        return NextResponse.json({
+            "message": "Something went wrong",
+            "type" : "Internal Server Error",
+            Error: error
+        }, {
+            status: 500
+        })
     }
 }
